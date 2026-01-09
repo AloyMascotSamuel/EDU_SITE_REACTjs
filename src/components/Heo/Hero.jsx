802ac3c6f922a4d1f815197e50fcd1917adf6b0c
Aloysius Samuel <111094446+AloyMascotSamuel@users.noreply.github.com>
2026-01-09 22:34:30 +0100

    added Hero component

---
+++ src/components/Heo/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    
+    <div className='hero'>
+        <div className="hero-text">
+            <h1>We ensure education for a better world</h1>
+            <p>Our cutting-edge programs are designed to empower students with the knowledge and skills they need to succeed in an ever-changing world.</p>
+            <button className="btn">Explore more</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
